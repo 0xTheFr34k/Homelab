1acc7583b247744c0a1ff9053f58ae4fadf110e6
0xTheFre34k <tayayassine6@live.fr>
2025-08-18 01:45:03 +0100

    3 nodes

--- proxmox.tf
+++ proxmox.tf
@@ -1,22 +1,59 @@
-resource "proxmox_vm_qemu" "resource-name" {
-  name        = "VM-name"
+variable "nodes" {
+  default = {
+    master = 1
+    worker = 2
+  }
+}
+
+# Master
+resource "proxmox_vm_qemu" "k8s_master" {
+  count       = var.nodes.master
+  name        = "k8s-master-${count.index}"
   target_node = "proxmox"
+
+  cpu {cores  = 2}
+  memory = 2048
 
   disks {
     ide {
       ide2 {
         cdrom {
-          iso = "ISO file"
+          iso = "local:iso/talos-metal-amd64.iso"
         }
       }
     }
   }
 
-  ### or for a Clone VM operation
-  # clone = "template to clone"
-
-  ### or for a PXE boot VM operation
-  # pxe = true
-  # boot = "scsi0;net0"
-  # agent = 0
+  network {
+    id = 0 
+    model  = "virtio"
+    bridge = "vmbr0"
+  }
 }
+
+# Workers
+resource "proxmox_vm_qemu" "k8s_worker" {
+  count       = var.nodes.worker
+  name        = "k8s-worker-${count.index}"
+  target_node = "proxmox"
+
+  cpu {cores  = 2}
+  memory = 2048
+
+  disks {
+    ide {
+      ide2 {
+        cdrom {
+          iso = "local:iso/talos-metal-amd64.iso"
+        }
+      }
+    }
+  }
+
+  network {
+    id = 0 
+    model  = "virtio"
+    bridge = "vmbr0"
+  }
+}
+
