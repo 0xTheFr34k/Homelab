58dfcbdda6e024b28b94f3185bfc0b74b39d3e88
0xTheFre34k <tayayassine6@live.fr>
2025-08-20 15:42:54 +0100

    add new node-0

--- terraform/proxmox.tf
+++ terraform/proxmox.tf
@@ -2,6 +2,7 @@ variable "nodes" {
   default = {
     master = 1
     worker = 2
+    node   = 1
   }
 }
 
@@ -28,7 +29,7 @@ resource "proxmox_vm_qemu" "k8s_master" {
     virtio {
       virtio0 {
         disk {
-          size    = "100G"
+          size    = "50G"
           storage = "local-lvm"
         }
       }
@@ -65,7 +66,42 @@ resource "proxmox_vm_qemu" "k8s_worker" {
     virtio {
       virtio0 {
         disk {
-          size    = "200G"
+          size    = "100G"
+          storage = "local-lvm"
+        }
+      }
+    }
+  }
+
+  network {
+    id     = 0
+    model  = "virtio"
+    bridge = "vmbr0"
+  }
+}
+
+resource "proxmox_vm_qemu" "node" {
+  count       = var.nodes.node
+  boot        = "order=virtio0;ide2;net0"
+  name        = "node-${count.index}"
+  target_node = "proxmox"
+  agent       = 1
+  cpu { cores = 4 }
+  memory = 4048
+  scsihw = "virtio-scsi-single"
+
+  disks {
+    ide {
+      ide2 {
+        cdrom {
+          iso = "local:iso/ubuntu-24.04.3-live-server-amd64.iso"
+        }
+      }
+    }
+    virtio {
+      virtio0 {
+        disk {
+          size    = "100G"
           storage = "local-lvm"
         }
       }
